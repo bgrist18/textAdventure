cdd66f461242abbc0923db97d403910ec6fe6c57
bgrist18 <bgrist309@gmail.com>
2016-09-14 11:46:18 -0400

    Start game code

--- src/game.java
+++ src/game.java
@@ -8,9 +8,23 @@ public class game {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome Traveler");
 
+        boolean keepRunning = true;
 
+
+        while (keepRunning);
         player player = new player();
         player.chooseName();
         player.chooseWeapon();
+        player.chooseLocation();
+
+        System.out.println("You win!");
+        System.out.println("Would you like to play again? Type y or n");
+
+        String answer = scanner.nextLine();
+        if (answer.equalsIgnoreCase("n")){
+            keepRunning = false;
+        }
+
+        System.out.println("Game Over");
     }
 }
