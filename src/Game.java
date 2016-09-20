c339ff7b9fb1e316be6b5dac5fb93581ba18e4c4
bgrist18 <bgrist309@gmail.com>
2016-09-20 11:57:44 -0400

    enemy vs character

--- src/Game.java
+++ src/Game.java
@@ -19,7 +19,9 @@ public class Game {
             player.findItem("shield");
             player.findItem("potion");
 
-            System.out.println("You win!");
+            Enemy ogre = new Enemy("Ogre", 10, 10);
+            player.battle(ogre);
+
             System.out.println("Would you like to play again? Type y or n");
 
             String answer = customLine();
