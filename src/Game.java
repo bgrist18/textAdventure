81e103c94c255b47cfdf38588e805a25f90feeda
bgrist18 <bgrist309@gmail.com>
2016-09-22 10:30:36 -0400

    json jodd

--- src/Game.java
+++ src/Game.java
@@ -1,39 +1,50 @@
+import jodd.json.JsonParser;
+import jodd.json.JsonSerializer;
+
+import java.beans.Customizer;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * Created by brett on 9/14/16.
+ * Created by john.tumminelli on 9/14/16.
  */
 public class Game {
-    static Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in); //moved scanner method to static
     static Player player = new Player();
+    static final String FILE_NAME = "game.json";
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome, traveller!");
+        System.out.println("Welcome, Traveller!");
 
         boolean keepRunning = true;
-
         while (keepRunning) {
+
+
             player.chooseName();
             player.chooseWeapon();
             player.chooseLocation();
             player.findItem("shield");
-            player.findItem("potion");
+            player.findItem("sword");
 
             Enemy ogre = new Enemy("Ogre", 10, 10);
             player.battle(ogre);
 
-            System.out.println("Would you like to play again? Type y or n");
+            System.out.println("You win!");
+            System.out.println("Would you like to play again? tpe y or n");
 
-            String answer = customLine();
+            String answer = customtLine();
             if (answer.equalsIgnoreCase("n")) {
                 keepRunning = false;
             }
         }
 
-        System.out.println("Game over.");
+        System.out.println("Game Over");
     }
 
-    static String customLine() {
+    static String customtLine() {
         String line = scanner.nextLine();
         while (line.startsWith("/")) {
             switch (line) {
@@ -48,12 +59,52 @@ public class Game {
                         System.out.println("You have no items.");
                     }
                     break;
+                case "/save":
+                    save();
+                    break;
+                case "/load":
+                    load();
+                    break;
+
                 default:
                     System.out.println("Invalid command!");
                     break;
+
+
+
+
             }
             line = scanner.nextLine();
         }
         return line;
+    }
+
+    static void save() {
+        JsonSerializer serializer = new JsonSerializer();
+
+        String json = serializer.deep(true).serialize(player);
+        File f = new File(FILE_NAME);
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write(json);
+            fw.close();
+        }catch (Exception e) {
+            System.out.println("Couldn't save to file!");
+        }
+    }
+
+    static void load() {
+        File f = new File(FILE_NAME);
+        try {
+            FileReader fr = new FileReader(f);
+            int fileSize = (int) f.length();     //cast to int
+            char[] contents = new char[fileSize];
+            fr.read(contents, 0, fileSize);
+            JsonParser parser = new JsonParser();
+            player = parser.parse(contents, Player.class);
+        }catch (Exception e) {
+            System.out.println("Couldn't load file!");
+        }
+
     }
 }
